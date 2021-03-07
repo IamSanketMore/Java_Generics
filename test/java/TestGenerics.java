81e67174d21d5c4677111ff5565a67912ff58d97
IamSanketMore <sanketmore.999.m@gmail.com>
2021-03-07 16:41:17 +0530

    Test case 3 floats for Find Maximum

--- test/java/TestGenerics.java
+++ test/java/TestGenerics.java
@@ -3,24 +3,44 @@ import org.junit.Test;
 
 public class TestGenerics
 {
-        MaxValueUsingGenerics findMax = new MaxValueUsingGenerics();
-
+    //------------------- Test for Integers ----------------------------------------
         @Test
         public void givenFirstNumberIsMaxNumberThenReturnFirstNumberAsMax() {
 
-            Integer maximum = findMax.maximumInteger(800, 70, 115);
+            Integer maximum = MaxValueUsingGenerics.maximumInteger(800, 70, 115);
             Assert.assertEquals((Integer) 800, maximum);
         }
 
         @Test
         public void givenSecondNumberIsMaXNumberThenReturnSecondNumberAsMaximum() {
-            Integer maximum1 = findMax.maximumInteger(5, 250, 112);
+            Integer maximum1 = MaxValueUsingGenerics.maximumInteger(5, 250, 112);
             Assert.assertEquals((Integer) 250, maximum1);
         }
 
         @Test
         public void givenThirdNumberIsMaXNumberThenReturnThirdNumberAsMaximum() {
-            Integer maximum2 = findMax.maximumInteger(109, 200, 999);
+            Integer maximum2 = MaxValueUsingGenerics.maximumInteger(109, 200, 999);
             Assert.assertEquals((Integer) 999, maximum2);
+        }
+
+        //------------------- Test for Float --------------------------
+
+        @Test
+        public void givenFloatFirstNumberIsMaxNumberThenReturnFirstNumberAsMax() {
+
+            Double maximum = MaxValueUsingGenerics.maximumFloat(14.3, 1.2, 2.3);
+            Assert.assertEquals((Double) 14.3, maximum);
+        }
+
+        @Test
+        public void givenFloatSecondNumberIsMaXNumberThenReturnSecondNumberAsMaximum() {
+            Double maximum = MaxValueUsingGenerics.maximumFloat(1.2,14.3, 2.3);
+            Assert.assertEquals((Double) 14.3, maximum);
+        }
+
+        @Test
+        public void givenFloatThirdNumberIsMaXNumberThenReturnThirdNumberAsMaximum() {
+            Double maximum = MaxValueUsingGenerics.maximumFloat( 1.2, 2.31,14.3);
+            Assert.assertEquals((Double) 14.3, maximum);
         }
 }
